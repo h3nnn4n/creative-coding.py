fcc153b83e8aec0353cf8192b86a6f18b4f8d074
h3nnn4n <uber.renan@gmail.com>
2018-10-19 22:09:52 -0300

    WIP, version check

--- src/test1.py
+++ src/test1.py
@@ -26,8 +26,17 @@ def draw_line_from_sequence(ctx, points, color):
     ctx.stroke()
 
 
-def circle_clip(ctx):
-    ctx.arc(width / 2, height / 2, min(height, width) * 0.25, 0, 2 * pi)
+def circle_clip(ctx, x=None, y=None, r=None):
+    if x is None:
+        x = width / 2
+
+    if y is None:
+        y = height / 2
+
+    if r is None:
+        r = min(x, y) * 0.25
+
+    ctx.arc(x, y, r, 0, 2 * pi)
     ctx.clip()
 
 
@@ -49,10 +58,6 @@ def set_background(ctx):
     ctx.rectangle(0, 0, width, height)
     ctx.fill()
 
-    ctx.set_source_rgb(0 / 255, 0 / 255, 0 / 255)
-    ctx.arc(width / 2, height / 2, min(height, width) * 0.25, 0, 2 * pi)
-    ctx.fill()
-
 
 def draw_lines(ctx, n_groups=3, n_lines=100):
     for _ in range(n_groups):
@@ -68,15 +73,53 @@ def draw_lines(ctx, n_groups=3, n_lines=100):
             update_points(points)
             draw_line_from_sequence(ctx, points, color)
 
-random_data = os.urandom(8)
-seed_value = int.from_bytes(random_data, byteorder="big")
-seed(seed_value)
-print('seed(%d)' % seed_value)
+
+def circle_thing(ctx, x=None, y=None, r=None):
+    if x is None:
+        x = width / 2
+
+    if y is None:
+        y = height / 2
+
+    if r is None:
+        r = min(x, y) * 0.5
+
+    # ctx.set_source_rgb(0 / 255, 0 / 255, 0 / 255)
+    # ctx.arc(x, y, r, 0, 2 * pi)
+    # ctx.fill()
+
+    circle_clip(ctx, x, y, r)
+    draw_lines(ctx, n_groups=20, n_lines=500)
+    ctx.reset_clip()
+
+
+# random_data = os.urandom(8)
+# seed_value = int.from_bytes(random_data, byteorder="big")
+# seed(seed_value)
+# print('seed(%d)' % seed_value)
+
+seed(729890378782812217)
 
 set_background(ctx)
-circle_clip(ctx)
-draw_lines(ctx, n_groups=20, n_lines=500)
-ctx.reset_clip()
+circle_thing(ctx)
+
+circles = [
+    (width * 0.16, height * 0.14, width * 0.11),
+    (width * 0.70, height * 0.12, width * 0.09),
+    (width * 0.46, height * 0.86, width * 0.04),
+    (width * 0.89, height * 0.52, width * 0.16),
+]
+
+for x, y, r in circles:
+    circle_thing(
+        ctx,
+        x=x,
+        y=y,
+        r=r
+    )
+
 draw_lines(ctx, n_lines=500)
+
+circle_thing(ctx, width * 0.12, height * 0.89, width * 0.04)
 
 surface.write_to_png('rectangle.png')
