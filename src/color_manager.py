0e8adf6ba858b1c9b6e4e8ef7b94bd7c1541eea6
h3nnn4n <uber.renan@gmail.com>
2018-10-21 13:49:52 -0300

    More colors + preview

--- src/color_manager.py
+++ src/color_manager.py
@@ -1,8 +1,11 @@
 class ColorManager:
     def __init__(self):
         self.colors = {
-            'cornsilk': (255, 248, 220),
-            'thistle': (216, 191, 216)
+            'cornsilk': (255, 248, 220),  # #fff8dc
+            'thistle': (216, 191, 216),  # #d8bfd8
+            'orange red': (255, 69, 0),  # #ff4500
+            'powder blue': (176, 224, 230),  # #b0e0e6
+            'steel blue': (70, 130, 180)  # #4682b4
         }
 
     def get_color(self, key, alpha=1.0, normalized=True):
